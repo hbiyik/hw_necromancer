5a957a5bcba6f513158159984601a02037ed925c
boogie <boogiepop@gmx.com>
2023-09-02 22:38:49 +0200

    unified remote server which works properly

---
+++ rock5b/unified-remote-server/remote.lua
@@ -0,0 +1,55 @@
+
+dragging = false;
+
+local mouse = libs.mouse;
+
+function update (r)
+	--server.update({id = "touch", text = r });
+end
+
+actions.down = function ()
+	update("down");
+end
+
+actions.up = function ()
+	update("up");
+end
+
+actions.tap = function ()
+	update("tap");
+	if (dragging) then
+		dragging = false;
+		mouse.dragend();
+		mouse.up();
+	else
+		mouse.down();
+		mouse.up();
+	end
+end
+
+actions.double = function ()
+	update("double");
+	mouse.double("left");
+end
+
+actions.hold = function ()
+	update("hold");
+	mouse.down();
+	mouse.dragbegin();
+	dragging = true;
+end
+
+actions.delta = function  (id, x, y)
+	update("delta: " .. x .. " " .. y);
+	mouse.moveraw(x, y);
+end
+
+actions.left = function ()
+	mouse.down();
+	mouse.up();
+end
+
+actions.right = function ()
+	mouse.down("right");
+	mouse.up("right");
+end
